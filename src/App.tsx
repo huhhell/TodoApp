d7f1499e1d2fa742e988c0ea5d9a9cdea558297e
huhhell <vika20020807@mail.ru>
2024-02-13 17:36:10 +0700

    create project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './null.css';
+import {useState} from "react";
+
+function App() {
+    const [categories, setCategories] = useState();
+    const [tasks, setTasks] = useState();
+
+    return <>
+
+    </>
+}
+
+export default App
