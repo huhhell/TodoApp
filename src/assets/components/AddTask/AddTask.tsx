3f4b1f13343626ef5801b36f970d5bbc75564022
huhhell <vika20020807@mail.ru>
2024-02-18 14:04:15 +0700

    add states for fields

--- src/assets/components/AddTask/AddTask.tsx
+++ src/assets/components/AddTask/AddTask.tsx
@@ -1,14 +1,28 @@
-import {ICategory} from "../../../App.tsx";
+import {ICategory, ITask} from "../../../App.tsx";
 import './addTask.scss'
+import React, {useState} from "react";
 
 interface IProps {
     categories: ICategory[];
+    addTask: (task: ITask) => void;
 }
-export default function AddTask({categories}: IProps) {
+export default function AddTask({categories, addTask}: IProps) {
+    const [taskName, setTaskName] = useState('');
+    const [category, setCategory] = useState('All Tasks');
+
+    function handleChangeCategory(e: React.ChangeEvent<HTMLSelectElement>) {
+        setCategory(e.target.value)
+        alert(category)
+    }
 
     return <div className='tasks__add'>
-        <input type="text" className="tasks__add-input" placeholder='Add a new task'/>
-        <select name="categories" id="categories" className="tasks__add-select">
+        <input
+            type="text"
+            className="tasks__add-input"
+            value={taskName}
+            placeholder='Add a new task'
+            onChange={(e) => setTaskName(e.target.value)}/>
+        <select name="categories" id="categories" className="tasks__add-select" onChange={handleChangeCategory}>
             {categories.map((i: ICategory) => <option value={i.name}
                                                       key={i.id}
                                                       className='tasks__add-option'>{i.name}</option>)}
